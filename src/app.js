33cfd1e4f777ccd50b66b47caca60e852170de38
Vijay <vijay.chichula@hitachiconsulting.com>
2020-05-06 00:55:22 +0530

    added changes related to port and Heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 const covidUtils = require('./utils/covid-utils')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -67,6 +68,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port.'+ post)
 })
